ce29200c19d5989411615f7191bb7426bd740dd9
irened123 <irenedeng7@gmail.com>
2024-07-26 18:11:36 -0400

    Implement Spotify Search and Fix Background Gradient
    - Integrated Spotify API to enable search functionality in the search bar.
    - Configured the search bar to query Spotify's /v1/search?type=TRACK endpoint.
    - Processed API responses and mapped them to track objects with id, name, artist, album, and uri properties.
    - Updated App.css to ensure the background gradient covers the entire viewport and remains consistent while scrolling.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const clientId = ''; // Insert client ID here.
+const clientId = '37e90207cccf48efabfe4f0737edb5a9'; // Insert client ID here.
 const redirectUri = 'http://localhost:3000/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
